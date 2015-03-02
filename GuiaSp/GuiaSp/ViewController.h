428c8b73b11ff2aa5d02e703077b80a6f7b4ce97
Amanda <amanda96campos@gmail.com>
2015-03-02 17:45:04 -0300

    commit

---
+++ GuiaSp/GuiaSp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuiaSp
+//
+//  Created by Amanda Guimaraes Campos on 02/03/15.
+//  Copyright (c) 2015 João Vitor dos Santos Schimmelpfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIScrollView *imagensScroll;
+@property (weak, nonatomic) IBOutlet UIPageControl *imagensPage;
+
+@end
---
+++ GuiaSp/GuiaSp/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  GuiaSp
+//
+//  Created by Amanda Guimaraes Campos on 02/03/15.
+//  Copyright (c) 2015 João Vitor dos Santos Schimmelpfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
